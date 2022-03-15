f26ecdb66b10f12de3bdae626aea3dc57ab91245
Sebastian Tobar Quintero <sebastianq@javerianacali.edu.co>
2022-03-15 18:50:20 +0000

    v1.0

--- code/main.py
+++ code/main.py
@@ -5,8 +5,9 @@ import socket
 import sys
 
 # Parametros
-ip = 'localhost'
-port = 10000
+ip = '127.0.0.1'
+port = 476 # Puerto por defecto de admin_container
+           # 476 - 490 Unofficial Centro Software ERP ports
 
 def set_ip(new_ip):
     global ip
@@ -21,22 +22,88 @@ def set_port(new_port):
 def connect_socket():
     global ip, port
 
-    return socket.create_connection(ip, port)
+    return socket.create_connection((ip, port))
 
-# Enviamos una petición al elastic-container-service por medio de un socket
-def send_petition(sock, petition):
+# Retorna si la operacion fue exitosa o no
+def create_container(sock, name):
     try:
-        message = str.encode(petition)
-        print('Solicitando petición nro. {!r}'.format(message))
+        order = "-c"
+        message = str.encode(order + name)
         sock.sendall(message)
 
         amount_received = 0
-        amount_expected = len(message)
+        amount_expected = 2
 
         while amount_received < amount_expected:
-            data = sock.recv(16)
+            data = sock.recv(512)
             amount_received += len(data)
-            print('Respuesta recibida: {!r}'.format(data))
+    except:
+        return False
     finally:
-        print('Cerrando socket')
-        sock.close()
+        if(int(data[2])):
+            return True
+        else:
+            return False
+
+# Retorna la lista de contenedores guardada en el admin_container
+def list_containers(sock):
+    try:
+        order = "-l"
+        message = str.encode(order)
+        sock.sendall(message)
+
+        amount_received = 0
+        amount_expected = 2
+
+        while amount_received < amount_expected:
+            data = sock.recv(512)
+            amount_received += len(data)
+    except Exception as e:
+        return "error" + str(e)
+    finally:
+        return data.decode("utf-8")
+
+# Retorna si el contenedor se detuvo o no
+def stop_container(sock, id):
+    try:
+        order = "-s"
+        message = str.encode(order  + id)
+        sock.sendall(message)
+
+        amount_received = 0
+        amount_expected = 2
+
+        while amount_received < amount_expected:
+            data = sock.recv(512)
+            amount_received += len(data)
+    except:
+        return False
+    finally:
+        if(int(data[2])):
+            return True
+        else:
+            return False
+
+# Retorna si el contenedor fue borrado o no
+def delete_container(sock, id):
+    try:
+        order = "-d"
+        message = str.encode(order + id)
+        sock.sendall(message)
+
+        amount_received = 0
+        amount_expected = 2
+
+        while amount_received < amount_expected:
+            data = sock.recv(512)
+            amount_received += len(data)
+    except:
+        return False
+    finally:
+        if(int(data[2])):
+            return True
+        else:
+            return False
+
+def close_connection(sock):
+    sock.close()
